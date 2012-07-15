e170cce80ba038f951bdff776ff51c240792ccb9
Alex Beal <alexlbeal@gmail.com>
2012-07-15 23:30:13 +0400

    Restructuring as python package.

---
+++ setup.py
@@ -0,0 +1,11 @@
+from distutils.core import setup
+
+setup(
+        name='LaMark',
+        version='0.1.0',
+        author='Alex Beal',
+        author_email='alexlbeal@gmail.com',
+        packages=['lamark', 'lamark.test'],
+        url='http://github.com/beala/lamark',
+        description='A tool to embedding LaTeX in Markdown',
+)
