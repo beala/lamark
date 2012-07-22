bd7540b44850377c23d42bebaccb6bd0a3386159
Alex Beal <alexlbeal@gmail.com>
2012-07-22 08:42:53 +0400

    Added entry point in setup.py
    1) The entry point will put `lamark` in your execution path when the
    package is installed with pip/easy_install/whatever.

--- setup.py
+++ setup.py
@@ -1,11 +1,16 @@
-from distutils.core import setup
+from setuptools import setup
 
 setup(
         name='LaMark',
-        version='0.1.0',
+        version='0.1.1',
         author='Alex Beal',
         author_email='alexlbeal@gmail.com',
         packages=['lamark', 'lamark.test'],
         url='http://github.com/beala/lamark',
         description='A tool to embedding LaTeX in Markdown',
+        entry_points = {
+            'console_scripts': [
+                'lamark = lamark.lamark:main'
+                ]
+            }
 )
